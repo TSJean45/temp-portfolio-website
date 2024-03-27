e7f8dfd2eefbdb98a3dae12bf770fc1ac5070ac6
JEAN\szuje <szujean45@gmail.com>
2024-03-27 09:50:34 +0800

    Init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Typography } from "@material-tailwind/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: "black", minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div className="" style={{ textAlign: "center", color: "white" }}>
+        <Typography variant="h1" style={{ fontSize: "4rem" }}>
+          Website under development
+        </Typography>
+        <Typography variant="subtitle1" style={{ marginTop: "20px" }}>
+          We are working hard to bring you an amazing experience. Stay tuned!
+        </Typography>
+      </div>
     </div>
   );
 }
